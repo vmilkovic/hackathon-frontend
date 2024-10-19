cd2db02a19043c5b22019417183413606b185622
Petar Cvetko Voćanec <cvetkovocanecpetar@gmail.com>
2024-10-19 20:06:20 +0200

    Added initial public home component

--- src/app/public/public-home/public-home.component.ts
+++ src/app/public/public-home/public-home.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-public-home',
   standalone: true,
+  imports: [RouterModule, ButtonModule],
   templateUrl: './public-home.component.html',
 })
 export class PublicHomeComponent {}
